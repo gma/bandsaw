1b5c0064df0b145674ad006963916cdc36f99ed1
Graham Ashton <graham@effectif.com>
2004-07-30 12:51:16 +0000

    Moved build process over to using gnome-common's gnome-autogen.sh.

--- bandsaw/autogen.sh
+++ bandsaw/autogen.sh
@@ -1,67 +1,23 @@
 #!/bin/sh
 # Run this to generate all the initial makefiles, etc.
-# This was lifted from the Gimp, and adapted slightly by
-# Raph Levien.
-#
-# Adapted (very slightly further) by Graham Ashton.
+                                                                                
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
 
-# Band Saw may build with older versions, I have problems with 1.8.
-WANT_AUTOMAKE=1.7
-
-DIE=0
-
-PROJECT=bandsaw
-
-(autoconf --version) < /dev/null > /dev/null 2>&1 || {
-	echo
-	echo "You must have autoconf installed to compile $PROJECT."
-	echo "Download the appropriate package for your distribution,"
-	echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
-	DIE=1
+REQUIRED_AUTOMAKE_VERSION=1.7
+                                                                                
+PKG_NAME="bandsaw"
+                                                                                
+(test -f $srcdir/configure.ac \
+  && test -f $srcdir/README \
+  && test -d $srcdir/src) || {
+    echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
+    echo " top-level $PKG_NAME directory"
+    exit 1
 }
-
-# Do we really need libtool?
-# (libtool --version) < /dev/null > /dev/null 2>&1 || {
-# 	echo
-# 	echo "You must have libtool installed to compile $PROJECT."
-# 	echo "Get ftp://ftp.gnu.org/pub/gnu/libtool-1.2.tar.gz"
-# 	echo "(or a newer version if it is available)"
-# 	DIE=1
-# }
-
-(automake --version) < /dev/null > /dev/null 2>&1 || {
-	echo
-	echo "You must have automake installed to compile $PROJECT."
-	echo "Get ftp://ftp.gnu.org/pub/gnu/automake-1.3.tar.gz"
-	echo "(or a newer version if it is available)"
-	DIE=1
+                                                                                
+which gnome-autogen.sh || {
+    echo "You need to install gnome-common from the GNOME CVS"
+    exit 1
 }
-
-if test "$DIE" -eq 1; then
-	exit 1
-fi
-
-if test -z "$*"; then
-	echo "I am going to run ./configure with no arguments - if you wish "
-        echo "to pass any to it, please specify them on the $0 command line."
-fi
-
-case $CC in
-*xlc | *xlc\ * | *lcc | *lcc\ *) am_opt=--include-deps;;
-esac
-
-for dir in .
-do 
-  echo processing $dir
-  (cd $dir; \
-      aclocalinclude="$ACLOCAL_FLAGS"; \
-  aclocal $aclocalinclude; \
-#   autoheader; \
-  automake --add-missing --gnu $am_opt; autoconf)
-done
-
-./configure "$@"
-
-echo 
-echo "Now type 'make install' to install $PROJECT or 'make check' to run " \
-     "the tests."
+USE_GNOME2_MACROS=1 USE_COMMON_DOC_BUILD=yes . gnome-autogen.sh
